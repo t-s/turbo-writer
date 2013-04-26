77427d8e341e31d99e2bfb4ea72430fd43e22d36
Lindsay Ridgeway <LDRidgeway@gmail.com>
2013-04-26 11:24:23 -0400

    Repair charset error for document download (refactored)

--- project/download_document.py
+++ project/download_document.py
@@ -4,13 +4,17 @@ from google.appengine.ext.webapp import blobstore_handlers
 
 class DownloadHandler(blobstore_handlers.BlobstoreDownloadHandler):
     def get(self, blob_key):
+        # Set header so that we can write Unicode body; note that headers must be
+        # byte strings, not unicode strings
+        self.response.content_type = 'charset=utf-8'
+
         blob_info = blobstore.BlobInfo.get(blob_key)
         if blob_info:
             try:
                 filename = self.request.get(u'filename')
                 self.send_blob(blob_key, save_as=filename)
-                self.response.write('Download successful')  # Writing Unicode string throws exception
+                self.response.write(u'Download successful')
             except:
-                self.response.write('Download unsuccessful')  # Writing Unicode string throws exception
+                self.response.write(u'Download unsuccessful')
         else:
-            self.response.write('Download unsuccessful')  # Writing Unicode string throws exception
+            self.response.write(u'Download unsuccessful')
