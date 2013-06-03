1583dab83f1fccff160cff8f1b59f8c8de105284
Lindsay Ridgeway <LDRidgeway@gmail.com>
2013-06-02 23:37:38 -0400

    List assignments and documents per variable

--- project/variable/index.py
+++ project/variable/index.py
@@ -18,6 +18,9 @@ class RequestHandler(webapp2.RequestHandler):
             variable[u'description'] = variable_entity.description
             variable[u'is_repeating'] = variable_entity.is_repeating
             variable[u'input_field'] = variable_entity.input_field
+            variable[u'assignments'] = self.get_assignments(project, variable)
+            variable[u'documents'] = self.get_documents(project, variable)
+
             variable_list.append(variable)
 
         # Create template and template values, render the page
@@ -27,3 +30,28 @@ class RequestHandler(webapp2.RequestHandler):
         jinja_template_values[u'variables'] = variable_list
 
         self.response.out.write(jinja_template.render(jinja_template_values))
+
+    def get_assignments(self, project, variable):
+        assignment_count = 0
+        assignments = u''
+        variable_name = variable["name"]
+        for assignment in dao.get_assignments(project):
+            if variable_name in assignment.variable_names:
+                assignment_count += 1
+                if len(assignments):
+                    assignments += u', '
+                assignments += assignment.name
+        if assignment_count > 1:
+            assignments = u'<span style="color: red;">{}</span>'.format(assignments)
+        return assignments
+
+    def get_documents(self, project, variable):
+        documents = u''
+        variable_name = variable["name"]
+        for document in dao.get_documents(project):
+            for document_item in dao.get_document_items(document):
+                if document_item.variable_name == variable_name:
+                    if len(documents):
+                        documents += u', '
+                    documents += document.name
+        return documents
--- template/variable/index.py
+++ template/variable/index.py
@@ -18,6 +18,8 @@ class RequestHandler(webapp2.RequestHandler):
             variable[u'description'] = variable_entity.description
             variable[u'is_repeating'] = variable_entity.is_repeating
             variable[u'input_field'] = variable_entity.input_field
+            variable[u'assignments'] = self.get_assignments(template, variable)
+            variable[u'documents'] = self.get_documents(template, variable)
             variable_list.append(variable)
 
         # Create template and template values, render the page
@@ -27,3 +29,29 @@ class RequestHandler(webapp2.RequestHandler):
         jinja_template_values[u'variables'] = variable_list
 
         self.response.out.write(jinja_template.render(jinja_template_values))
+
+    def get_assignments(self, template, variable):
+        assignment_count = 0
+        assignments = u''
+        variable_name = variable["name"]
+        for assignment in dao.get_assignments(template):
+            if variable_name in assignment.variable_names:
+                assignment_count += 1
+                if len(assignments):
+                    assignments += u', '
+                assignments += assignment.name
+        if assignment_count > 1:
+            assignments = u'<span style="color: red;">{}</span>'.format(assignments)
+        return assignments
+
+    def get_documents(self, template, variable):
+        documents = u''
+        variable_name = variable["name"]
+        for document in dao.get_documents(template):
+            for document_item in dao.get_document_items(document):
+                if document_item.variable_name == variable_name:
+                    if len(documents):
+                        documents += u', '
+                    documents += document.name
+        return documents
+
